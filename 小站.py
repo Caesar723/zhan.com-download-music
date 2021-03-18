00800540fdc8c7a25fcce9e03bd560e37e3e3eca
caesar <76422688+Caesar723@users.noreply.github.com>
2021-03-18 20:36:40 +0800

    Update 小站.py

--- 小站.py
+++ 小站.py
@@ -12,9 +12,9 @@ def mp3(name,range):
         f.write(img)
 def beautiful(name):
     url=name
-    header = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
-              'cookie': " _ga=GA1.2.148899270.1613441373; _gat=1; _gid=GA1.2.28517347.1616058633; viptellfromwhere=fa6ebcdc5528966517ba435575cf062cc151f8c7f8ae8ebd0bd6a3c7387f9091a%3A2%3A%7Bi%3A0%3Bs%3A16%3A%22viptellfromwhere%22%3Bi%3A1%3Bi%3A1%3B%7D; PHPSESSID=42ru9h24n377s8moonlm362nl3"
-
+    header = {'user-agent': "",
+              'cookie': " "
+#input your cookie and agent
               }
     req = request.Request(url, headers=header)
     response = request.urlopen(req)
@@ -28,8 +28,8 @@ def beautiful(name):
     return get[0]
 def bighttp(name):
     url = name
-    header = {'user-agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Safari/605.1.15",
-              'cookie': " _ga=GA1.2.148899270.1613441373; _gat=1; _gid=GA1.2.28517347.1616058633; viptellfromwhere=fa6ebcdc5528966517ba435575cf062cc151f8c7f8ae8ebd0bd6a3c7387f9091a%3A2%3A%7Bi%3A0%3Bs%3A16%3A%22viptellfromwhere%22%3Bi%3A1%3Bi%3A1%3B%7D; PHPSESSID=42ru9h24n377s8moonlm362nl3"
+    header = {'user-agent': "",
+              'cookie': ""
               }
     req = request.Request(url, headers=header)
     response = request.urlopen(req)
@@ -38,7 +38,7 @@ def bighttp(name):
     be = str(BeautifulSoup(data, "html.parser"))
     find=re.findall(r"/work.{39,50}3D",be)
     return find
-get=bighttp("https://vip.zhan.com/work/homework/info.html?workId=1006718")
+get=bighttp("https://vip.zhan.com/work/homework/info.html?workId=1006718")#input http
 for i in range(0,len(get)):
     http=str("https://vip.zhan.com"+get[i]).replace('§','&sect')
     gethttp=beautiful(http)
